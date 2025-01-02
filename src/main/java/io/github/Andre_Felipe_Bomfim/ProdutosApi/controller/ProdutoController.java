1ef88f8649d8464417df3c3884b8e7a9a30b11b9
andre <strongerandrebomfim@gmail.com>
2025-01-02 19:54:22 -0300

    Update ProdutoController push @RequestBody

--- src/main/java/io/github/Andre_Felipe_Bomfim/ProdutosApi/controller/ProdutoController.java
+++ src/main/java/io/github/Andre_Felipe_Bomfim/ProdutosApi/controller/ProdutoController.java
@@ -2,6 +2,7 @@ package io.github.Andre_Felipe_Bomfim.ProdutosApi.controller;
 
 import io.github.Andre_Felipe_Bomfim.ProdutosApi.model.Produto;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("products")
 public class ProdutoController {
     @PostMapping("/post-product")
-    public void save(Produto produto){
+    public void save(@RequestBody Produto produto){
         System.out.println("Produto recebido: " + produto);
     }
 }
