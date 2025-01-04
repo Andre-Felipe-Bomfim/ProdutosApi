b77e5a5836d6a2a95e2eafea186b16739d1e61f6
andre <strongerandrebomfim@gmail.com>
2025-01-03 22:41:58 -0300

    Make a Method List<>

--- src/main/java/io/github/Andre_Felipe_Bomfim/ProdutosApi/repository/ProdutoRepository.java
+++ src/main/java/io/github/Andre_Felipe_Bomfim/ProdutosApi/repository/ProdutoRepository.java
@@ -3,6 +3,8 @@ package io.github.Andre_Felipe_Bomfim.ProdutosApi.repository;
 import io.github.Andre_Felipe_Bomfim.ProdutosApi.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProdutoRepository extends JpaRepository<Produto, String> {
+import java.util.List;
 
+public interface ProdutoRepository extends JpaRepository<Produto, String> {
+    List<Produto> findByName(String name);
 }
